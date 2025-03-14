3cb4b2174bb10c070803fb63710361438e1cd56f
Renato <renatoconrado38@gmail.com>
2025-03-14 14:15:32 -0300

    return

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,5 @@ import type { CustomWindow, Pages } from "./types";
 declare let window: CustomWindow;
 
 window.stupidFunction = (page: Pages) => {
-  location.href = `/playground/pages/${page}/`
+  location.href = `/pages/${page}/`
 };
